67858cdf5b6ca241da4290c24ddbd18544fa4adf
cassiadev <37288667+cassiadev@users.noreply.github.com>
2023-04-13 21:04:08 +0900

    Created a pomodoro app
    BorderRadius, Timer.periodic, Duration

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/screens/home_screen.dart';
 import 'package:toonflix/widgets/button.dart';
 import 'package:toonflix/widgets/currency_card.dart';
 
@@ -6,105 +7,121 @@ void main() {
   runApp(App());
 }
 
-class App extends StatefulWidget{
-  @override
-  State<App> createState() => _AppState();
-}
+// class App extends StatefulWidget{
+//   @override
+//   State<App> createState() => _AppState();
+// }
 
-class _AppState extends State<App> {
-  bool showTitle = true;
-  void toggleTitle() {
-    setState(() {
-      showTitle = !showTitle;
-    });
-  }
-
-  int counter = 0;
-  List<int> numbers = [];
-  void onClicked() {
-    setState(() { // State에 바뀐 데이터를 적용해서 화면에 표시하기 위해 setState() 함수가 꼭 필요함. 없으면 build()가 다시 실행되지 않아 연산에 의해 데이터가 변경중인데 화면에는 보이지 않게 됨
-      counter++;  // 꼭 setState() 안에 있어야지만 Interactivity가 적용되는 것은 아니나, 가독성 면에서 봤을 때 안에 넣는 게 좋다
-      numbers.add(numbers.length);
-    });
-  }
-
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
+        backgroundColor: const Color(0xFFE7626C),
         textTheme: const TextTheme(
-          titleLarge: TextStyle(
-           color: Colors.red,
-         ),
-        ),
-      ),
-      home: Scaffold(
-        backgroundColor: const Color(0xFFF4EDDB),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              showTitle ? const MyLargeTitle() : const Text('nothing to show'),
-              IconButton(
-                onPressed: toggleTitle,
-                icon: const Icon(Icons.remove_red_eye),
-              ),
-              Text('$counter',
-                style: const TextStyle(
-                  fontSize: 30,
-                ),
-              ),
-              for(var n in numbers) Text('$n',),
-              IconButton(
-                iconSize: 40,
-                onPressed: onClicked,
-                icon: const Icon(
-                    Icons.add_box_rounded,
-                ),
-              ),
-            ],
+          headline1: TextStyle(
+            color: Color(0xFF232B55),
           ),
         ),
+        cardColor: const Color(0xFFF4EDDB),
       ),
+      home: const HomeScreen(),
     );
   }
+
+  // bool showTitle = true;
+  // void toggleTitle() {
+  //   setState(() {
+  //     showTitle = !showTitle;
+  //   });
+  // }
+  //
+  // int counter = 0;
+  // List<int> numbers = [];
+  // void onClicked() {
+  //   setState(() { // State에 바뀐 데이터를 적용해서 화면에 표시하기 위해 setState() 함수가 꼭 필요함. 없으면 build()가 다시 실행되지 않아 연산에 의해 데이터가 변경중인데 화면에는 보이지 않게 됨
+  //     counter++;  // 꼭 setState() 안에 있어야지만 Interactivity가 적용되는 것은 아니나, 가독성 면에서 봤을 때 안에 넣는 게 좋다
+  //     numbers.add(numbers.length);
+  //   });
+  // }
+  //
+  // @override
+  // Widget build(BuildContext context) {
+  //   return MaterialApp(
+  //     theme: ThemeData(
+  //       textTheme: const TextTheme(
+  //         titleLarge: TextStyle(
+  //          color: Colors.red,
+  //        ),
+  //       ),
+  //     ),
+  //     home: Scaffold(
+  //       backgroundColor: const Color(0xFFF4EDDB),
+  //       body: Center(
+  //         child: Column(
+  //           mainAxisAlignment: MainAxisAlignment.center,
+  //           children: [
+  //             showTitle ? const MyLargeTitle() : const Text('nothing to show'),
+  //             IconButton(
+  //               onPressed: toggleTitle,
+  //               icon: const Icon(Icons.remove_red_eye),
+  //             ),
+  //             Text('$counter',
+  //               style: const TextStyle(
+  //                 fontSize: 30,
+  //               ),
+  //             ),
+  //             for(var n in numbers) Text('$n',),
+  //             IconButton(
+  //               iconSize: 40,
+  //               onPressed: onClicked,
+  //               icon: const Icon(
+  //                   Icons.add_box_rounded,
+  //               ),
+  //             ),
+  //           ],
+  //         ),
+  //       ),
+  //     ),
+  //   );
+  // }
 }
 
-class MyLargeTitle extends StatefulWidget {
-  const MyLargeTitle({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  State<MyLargeTitle> createState() => _MyLargeTitleState();
-}
-
-class _MyLargeTitleState extends State<MyLargeTitle> {
-  int count = 0;
-
-  @override
-  void initState() {  // initState()는 부모 요소에 의존하는 데이터의 초기화를 위해 종종 사용됨. 특별한 목적이 없으면 그냥 클래스 변수 선언을 해도 됨.
-    print('initState!');
-    super.initState();  // initState()는 항상 build()보다 먼저 호출되어야 하며 단 한 번만 호출됨. super.initState()도 있어야 됨
-  }
-
-  @override
-  void dispose() {  // dispose()는 위젯이 스크린에서 또 위젯트리에서 제거될 때 호출됨
-    print('dispose!');
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {  // context에는 위젯트리에 관한 정보, 즉 조상 요소들의 데이터가 모두 담겨 있다
-    print('build!');
-    return Text('My Large Title',
-      style: TextStyle(
-        fontSize: 30,
-        color: Theme.of(context).textTheme.titleLarge?.color, // titleLarge라는 것이 없을 때에 대비해 ?.나 아니면 확실히 있다는 의미로 !.를 붙여준다
-      ),
-    );
-  }
-}
+// class MyLargeTitle extends StatefulWidget {
+//   const MyLargeTitle({
+//     Key? key,
+//   }) : super(key: key);
+//
+//   @override
+//   State<MyLargeTitle> createState() => _MyLargeTitleState();
+// }
+//
+// class _MyLargeTitleState extends State<MyLargeTitle> {
+//   int count = 0;
+//
+//   @override
+//   void initState() {  // initState()는 부모 요소에 의존하는 데이터의 초기화를 위해 종종 사용됨. 특별한 목적이 없으면 그냥 클래스 변수 선언을 해도 됨.
+//     print('initState!');
+//     super.initState();  // initState()는 항상 build()보다 먼저 호출되어야 하며 단 한 번만 호출됨. super.initState()도 있어야 됨
+//   }
+//
+//   @override
+//   void dispose() {  // dispose()는 위젯이 스크린에서 또 위젯트리에서 제거될 때 호출됨
+//     print('dispose!');
+//     super.dispose();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {  // context에는 위젯트리에 관한 정보, 즉 조상 요소들의 데이터가 모두 담겨 있다
+//     print('build!');
+//     return Text('My Large Title',
+//       style: TextStyle(
+//         fontSize: 30,
+//         color: Theme.of(context).textTheme.titleLarge?.color, // titleLarge라는 것이 없을 때에 대비해 ?.나 아니면 확실히 있다는 의미로 !.를 붙여준다
+//       ),
+//     );
+//   }
+// }
 
 // class App extends StatelessWidget {
 //   @override
